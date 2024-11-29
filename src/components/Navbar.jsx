5611c63f0f8bbdf8812a732475f25efe84e90e29
Kasador <huntershaw23@gmail.com>
2024-11-29 10:47:56 -0800

    navbar + HERO >>> BIG UPDATE <<< changed positioning, added logo w/ fixes, change background color on button to make it look like a button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HiChevronDown } from "react-icons/hi"; // for drow down arrows
 
 const Navbar = () => {
   const toggleTheme = () => {
@@ -6,10 +7,10 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-light-primary dark:bg-dark-primary text-light-background dark:text-dark-text px-4 py-5">
+    <nav className="bg-light-primary dark:bg-dark-primary text-light-background dark:text-dark-text px-4 py-2">
       <div className="container mx-auto flex items-center justify-between">
         {/* Main Menu */}
-        <ul className="flex space-x-6 z-10">
+        <ul className="flex space-x-4 z-10 items-center">
           <li>
             <a href="/" className="hover:bg-light-accent p-3 rounded-md dark:hover:bg-dark-accent duration-100">
               Home
@@ -21,11 +22,11 @@ const Navbar = () => {
             </a>
           </li>
           <li className="group relative">
-            <a href="/resources" className="hover:bg-light-accent p-3 rounded-md dark:hover:text-dark-accent duration-100">
-              Resources
+            <a href="/resources" className="hover:bg-light-accent p-3 rounded-md flex items-center dark:hover:text-dark-accent duration-100">
+              Resources <HiChevronDown />
             </a>
             {/* Dropdown Menu - Resources */}
-            <ul className="absolute hidden group-hover:block bg-light-surface dark:bg-dark-surface text-sm mt-2 shadow-lg rounded text-light-text">
+            <ul className="absolute hidden group-hover:block bg-light-surface dark:bg-dark-surface text-md shadow-lg rounded text-light-text">
               <li>
                 <a
                   href="/resources/nevada-veterans"
@@ -77,11 +78,11 @@ const Navbar = () => {
             </ul>
           </li>
           <li className="group relative">
-            <a href="/gallery" className="hover:bg-light-accent p-3 rounded-md dark:hover:text-dark-accent duration-100">
-              Gallery
+            <a href="/gallery" className="hover:bg-light-accent p-3 rounded-md flex items-center dark:hover:text-dark-accent duration-100">
+              Gallery <HiChevronDown />
             </a>
             {/* Dropdown Menu - Gallery */}
-            <ul className="absolute hidden group-hover:block bg-light-surface dark:bg-dark-surface text-sm mt-2 shadow-lg rounded text-light-text">
+            <ul className="absolute hidden group-hover:block bg-light-surface dark:bg-dark-surface text-md shadow-lg rounded text-light-text">
               <li>
                 <a
                   href="/gallery/photos"
