a4125af14d6ef18c37e448a2df05065f9e848db3
Kasador <huntershaw23@gmail.com>
2024-11-29 08:58:56 -0800

    main >>> Updated BG image and positioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 // import { useState } from 'react'
 import Header from "./components/Header.jsx";
 import HeroImage from "./components/HeroImage.jsx";
-import Navbar from "./components/Navbar.jsx";
+// import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <HeroImage />
-    <main className="bg-light-background">
-      <h1>Test</h1>
-    </main>
+      <Header />
+      <HeroImage />
     </>
   )
 }
--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const HeroImage = () => {
     return (
-      <div className="absolute h-screen w-full bg-cover bg-center z-0" style={{ backgroundImage: "url('./src/test/test.png')" }}> {/* the image + wrapper */}
-        <div className="absolute inset-0 bg-black bg-opacity-65 flex items-center justify-center"> {/* wrapper for hero content + flex items */}
+      <div className="absolute h-full w-full object-cover bg-center bg-no-repeat z-0 bg-light-surface" style={{ backgroundImage: "url('./src/test/navadavetsorg-hero-bg.jpg')" }}> {/* the image + wrapper */}
+        <div className="absolute inset-0 flex items-center justify-center"> {/* wrapper for hero content + flex items */}
           <div className="text-center text-white px-4"> {/* Wrapper + content */}
             <h1 className="text-4xl md:text-6xl font-bold">Welcome to Our Site</h1>
             <p className="mt-4 text-lg md:text-xl">Your tagline or hero message goes here.</p>
