a6f40782ab39a4b54429723c4ec6a0779d43dcc8
Kasador <huntershaw23@gmail.com>
2024-11-29 19:20:08 -0800

    main >>> Update hero image logo/text/button <<< positioning fixed

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -4,10 +4,10 @@ import { HiChevronDown } from "react-icons/hi";
 
 const HeroImage = () => {
     return (
-      <div className="absolute h-full w-5/6 object-cover bg-center bg-no-repeat z-0 bg-light-surface top-20" style={{ backgroundImage: "url('./src/test/navadavetsorg-hero-bg.jpg')" }}> {/* the image + wrapper */}
-        <div className="absolute inset-0 flex items-center justify-center"> {/* wrapper for hero content + flex items */}
+      <div className="absolute h-full w-5/6 object-cover bg-top bg-no-repeat z-0 bg-light-surface top-32" style={{ backgroundImage: "url('./src/test/navadavetsorg-hero-bg.jpg')" }}> {/* the image + wrapper */}
+        <div className="relative top-20 flex items-center justify-center"> {/* wrapper for hero content + flex items */}
           <div className="text-center text-white px-4"> {/* Wrapper + content */}
-          <div style={{ backgroundImage: "url('./src/test/nevadavet-logo-main.png')"}} className="h-32 w-32 bg-contain bg-center bg-no-repeat mx-auto relative right-2"></div>
+            <div style={{ backgroundImage: "url('./src/test/nevadavet-logo-main.png')"}} className="h-32 w-32 bg-contain bg-center bg-no-repeat mx-auto relative right-2"></div>
             <h1 className="text-4xl md:text-6xl font-bold">Nevada Veterans Foundations</h1>
             <p className="mt-4 text-lg md:text-xl">A non-profit that supports our own.</p>
             <button className="mt-6 px-6 py-3 bg-light-accent text-white font-medium rounded-lg hover:bg-opacity-80">
